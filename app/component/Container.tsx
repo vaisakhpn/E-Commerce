a0dbd5735a87f9b1f84687ec4afd7fb232b7f645
Vaisakh <vaisakhpn78@gmail.com>
2024-01-07 22:12:16 +0530

    added container

---
+++ app/component/Container.tsx
@@ -0,0 +1,11 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="max-w-6xl mx-auto xl:px-20 md:px-8 px-4">{children}</div>
+  );
+};
+
+export default Container;
