2adf8f4e7e4312bff91d1cba50561350f3001aa2
Vaisakh <vaisakhpn78@gmail.com>
2024-01-08 00:07:59 +0530

    Added Banner into home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Container from "./component/Container";
+import HomeBanner from "./component/HomeBanner";
+
 export default function Home() {
   return (
-    <div >
-     <h1>E-Commerce</h1>
+    <div className="p-8" >
+     <Container>
+      <div>
+        <HomeBanner/>
+      </div>
+     </Container>
     </div>
   )
 }
