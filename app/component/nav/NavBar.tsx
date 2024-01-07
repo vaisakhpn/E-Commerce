5d07c0c025c6c8eea46023ead6205119affbeb17
Vaisakh <vaisakhpn78@gmail.com>
2024-01-07 22:21:32 +0530

    Navbar designed

--- app/component/nav/NavBar.tsx
+++ app/component/nav/NavBar.tsx
@@ -1,6 +1,26 @@
+import Link from "next/link";
+import Container from "../Container";
+import { Redressed } from "next/font/google";
 
-export default function NavBar() {
+const redressed =Redressed({subsets:['latin'],weight:['400']});
+
+const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Link className={`${redressed.className} font-bold text-2xl`} href="/">E-Commerce</Link>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div>Cart</div>
+              <div>Profile</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
