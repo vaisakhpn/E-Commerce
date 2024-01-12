432bf80f20484f7a36d8fb0a526079a352605d67
Vaisakh <vaisakhpn78@gmail.com>
2024-01-12 20:12:40 +0530

    Added product review section

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,16 +1,24 @@
 import Container from "@/app/component/Container";
 import ProductDetails from "./ProductDetails";
 import { product } from "@/utils/product";
+import ListRating from "./ListRating";
 
-interface IParams{
-    productId?:string;
+interface IParams {
+  productId?: string;
 }
 
-const Product = ({params}:{params:IParams}) => {
-   
-    return (<div className="p-8"><Container>
+const Product = ({ params }: { params: IParams }) => {
+  return (
+    <div className="p-8">
+      <Container>
         <ProductDetails product={product} />
-        </Container></div>  );
-}
- 
+        <div className="flex flex-col mt-20 gap-4">
+          <div>Add Rating</div>
+          <ListRating product={product} />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
 export default Product;
