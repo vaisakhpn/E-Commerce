c7428ed02424b7ac0971558d7454c43a7c9ad1c8
Vaisakh <vaisakhpn78@gmail.com>
2024-05-24 19:14:35 +0530

    flex col

--- app/component/footer/FooterList.tsx
+++ app/component/footer/FooterList.tsx
@@ -4,7 +4,7 @@ interface FooterListProps {
 
 const FooterList: React.FC<FooterListProps> = ({ children }) => {
   return (
-    <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/6 mb-6 flex flex-row gap-2">
+    <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/6 mb-6 flex flex-col gap-2">
       {children}
     </div>
   );
