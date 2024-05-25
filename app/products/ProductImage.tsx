a6ce9b23637c8cb558c2e067125efe0971f39bdc
Vaisakh <vaisakhpn78@gmail.com>
2024-05-25 19:11:10 +0530

    image size

--- app/products/ProductImage.tsx
+++ app/products/ProductImage.tsx
@@ -18,7 +18,7 @@ const ProductImage: React.FC<ProductImageProps> = ({
   handleColorSelect,
 }) => {
   return (
-    <div className="grid grid-cols-6 gap-2 h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
+    <div className="grid grid-cols-6 gap-2 h-full max-h-[400px] min-h-[300px] sm:min-h-[400px]">
       <div className="flex flex-col items-center justify-center gap-4 cursor-pointer border h-full max-h-[500px] min-h-[300px] sm:min-h-[400px] ">
         {product.images.map((image: SelectedImgType) => {
           return (
