1f086cee21db2fc5f2ff417c0b803f042f4742c8
Vaisakh <vaisakhpn78@gmail.com>
2024-05-31 22:04:46 +0530

    modified nav

--- app/component/nav/NavBar.tsx
+++ app/component/nav/NavBar.tsx
@@ -2,15 +2,20 @@ import Link from "next/link";
 import Container from "../Container";
 import { Redressed } from "next/font/google";
 
-const redressed =Redressed({subsets:['latin'],weight:['400']});
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
 const NavBar = () => {
   return (
     <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
-      <div className="py-4 border-b-[1px]">
+      <div className="py-1 border-b-[1px]">
         <Container>
-        <div className="flex items-center justify-between gap-3 md:gap-0 select-none">
-            <Link className={`${redressed.className} font-bold text-2xl`} href="/">E-Commerce</Link>
+          <div className="flex items-center justify-between gap-3 md:gap-0 select-none">
+            <Link
+              className={`${redressed.className} font-bold text-2xl`}
+              href="/"
+            >
+              E-Commerce
+            </Link>
             <div className="hidden md:block">Search</div>
             <div className="flex items-center gap-8 md:gap-12 cursor-pointer">
               <div>Cart</div>
