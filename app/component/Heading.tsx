4a5af37040932b2116c649a5a6de3b5d07baee2e
Vaisakh <vaisakhpn78@gmail.com>
2024-06-04 19:03:30 +0530

    Header modified

--- app/component/Heading.tsx
+++ app/component/Heading.tsx
@@ -5,7 +5,9 @@ interface HeadingProps {
 
 const Heading: React.FC<HeadingProps> = ({ title, center }) => {
   return (
-
+    <div className={center ? "text-center" : "text-start"}>
+      <h1 className="font-bold text-2xl">{title}</h1>
+    </div>
   );
 };
 
